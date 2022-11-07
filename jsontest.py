8c508a0e0efb1c106b8cd9419af94f0e584c47e1
Wesley Preuss <wespreuss@gmail.com>
2022-11-07 16:34:20 +0000

    Orders are now on a json

--- jsontest.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-def open_courier_db():
-    with open("mini_project_folder/test.json","r") as test_db:
-        input_data = test_db.read()
-        output_data = json.loads(input_data)
-        return(output_data)
-
